895114d72bbfdabc6597da95698b3269793feb79
Ronald Sarmiento <ronicuentasbn@gmail.com>
2020-05-09 14:19:31 -0400

    model many to many with class Tag

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -22,14 +22,25 @@ class Product(models.Model):
    description =  models.CharField(max_length=200, null=True)
    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
+class Tag (models.Model):
+   name = models.CharField(max_length=200, null=True)
+      
+   def __str__(self):
+      return self.name
+   
 class Order (models.Model):
    STATUS = (
       ('Pending','Pending'),
       ('Out for delivery','Out for delivery'),
       ('Delivered','Delivered'),
       )
+   customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+   product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
    date_created = models.DateTimeField(auto_now_add=True, null=True)
    status = models.CharField(max_length=200, null=True, choices=STATUS)
+   tag = models.ManyToManyField(Tag)
+   
+
    
 	
 
