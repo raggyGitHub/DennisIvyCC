767df9c090f73b7b84e6725e800470cf98f54910
Ronald Sarmiento <ronicuentasbn@gmail.com>
2020-05-10 19:47:48 -0400

    create update delete with ModelForms

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 
 from .models import *
-
+from .forms import OrderForm
 # Create your views here.
 
 def home(request):
@@ -33,3 +33,43 @@ def customers(request, pk_test):
    
    context = {'customer':customer,'orders':orders,'order_count':order_count}
    return render(request, "accounts/customers.html",context)
+
+#crud update and delete  views
+def createOrder(request):
+   
+   form = OrderForm
+   if request.method == 'POST':
+      
+      form = OrderForm(request.POST)
+      if form.is_valid():
+         form.save()
+         return redirect('/')
+   
+   context = { 'form':form }
+   return render (request,'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+   
+   order = Order.objects.get(id = pk)
+   form = OrderForm(instance = order)
+   
+   if request.method == 'POST':
+      
+      form = OrderForm(request.POST, instance = order)
+      if form.is_valid():
+         form.save()
+         return redirect('/')
+   context = {'form':form}
+   return render (request,'accounts/order_form.html', context)
+
+def deleteOrder(request,pk):
+   
+   order = Order.objects.get(id = pk)
+   
+   if request.method == 'POST':
+      order.delete()
+      return redirect('/')
+   
+   context = {'item':order}
+   return render (request, 'accounts/delete.html', context)
+   
