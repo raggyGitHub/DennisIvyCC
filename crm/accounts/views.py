843f3d54d921134e7430bbdd2465996a3443350d
Ronald Sarmiento <ronicuentasbn@gmail.com>
2020-05-11 13:49:50 -0400

    Inline Formsets

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 
 from .models import *
 from .forms import OrderForm
@@ -25,7 +26,7 @@ def products(request):
    products = Product.objects.all()
    return render(request, "accounts/products.html",{'listproducts':products})
 
-def customers(request, pk_test):
+def customer(request, pk_test):
    customer = Customer.objects.get(id=pk_test)
    
    orders = customer.order_set.all()
@@ -35,17 +36,18 @@ def customers(request, pk_test):
    return render(request, "accounts/customers.html",context)
 
 #crud update and delete  views
-def createOrder(request):
-   
-   form = OrderForm
+def createOrder(request, pk ):
+   OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status'),extra = 10)
+   customer = Customer.objects.get(id = pk)
+   formset = OrderFormSet(queryset=Order.objects.none ,instance = customer)
+   #form = OrderForm( initial = {'customer': customer }) this line changed using inline Formsets
    if request.method == 'POST':
-      
       form = OrderForm(request.POST)
-      if form.is_valid():
-         form.save()
+      formset = OrderFormSet(request.POST, instance = customer)
+      if formset.is_valid():
+         formset.save()
          return redirect('/')
-   
-   context = { 'form':form }
+   context = { 'formset':formset }
    return render (request,'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
